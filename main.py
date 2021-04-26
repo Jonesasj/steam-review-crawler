25fc96e98a41e45d35784318231335ff56d0bead
alex jones <alexsjones@protonmail.com>
2021-04-26 03:06:08 +0100

    Added functionality to save reviews to a file

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import urllib.request
 import json
 from review_handler import ReviewHandler
 
+cursor = '*'
 REVIEW_SIZE_LIMIT = 5000
 franchise = "temp franchise name"
 gameName = "temp game name"
-url = "https://store.steampowered.com/appreviews/1382330?json=1"
+
+url = "https://store.steampowered.com/appreviews/1382330?json=1&num_per_page=100"
 
 req = urllib.request.Request(url=url, method='GET')
 with urllib.request.urlopen(req) as response:
@@ -13,14 +15,17 @@ with urllib.request.urlopen(req) as response:
 
 #print(data)
 jsonData = json.loads(data)
+print(jsonData.get('cursor'))
 
 reviews = []
 #check if the query is successful
 if jsonData.get('success') == 1:
     reviews = jsonData.get('reviews')
+    print(len(reviews))
 
-handler = ReviewHandler(reviews)
-handler.somename()
+handler = ReviewHandler(franchise, gameName)
+handler.addReviews(reviews)
+handler.saveReviews('test.json')
 
 #for i in range(len(jsonData)):
 #    print(jsonData[i])
--- review_handler.py
+++ review_handler.py
@@ -1,26 +1,26 @@
 import uuid
 from datetime import date
+import json
 
 class ReviewHandler:
-    def __init__(self, reviews, franchise, gameName):
-        self.reviews = reviews
+    def __init__(self, franchise, gameName):
+        self.reviews = []
         self.franchise = franchise
         self.gameName = gameName
 
-    def somename(self):
+    def addReviews(self, reviews):
         reviewList = []
-        for i in range(len(self.reviews)):
+        for i in range(len(reviews)):
             if i == 3:
-                thisReview = self.reviews[i]
-                print(self.reviews[i])
-                print(self.reviews[i].get('recommendationid'))
-                print(date.fromtimestamp(thisReview.get('timestamp_created')))
+                thisReview = reviews[i]
+                print(reviews[i])
+                print(reviews[i].get('recommendationid'))
 
                 review = {}
                 review['id'] = thisReview.get('recommendationid')
-                review['author'] = uuid.uuid3(uuid.NAMESPACE_URL, thisReview.get('author').get('steamid'))
-                review['date'] = date.fromtimestamp(thisReview.get('timestamp_created'))
-                review['hours'] = thisReview.get('playtime_at_review')/60 #Using playtime at review because it's more useful than playtime_forever
+                review['author'] = str(uuid.uuid3(uuid.NAMESPACE_URL, thisReview.get('author').get('steamid')))
+                review['date'] = str(date.fromtimestamp(thisReview.get('timestamp_created')))
+                review['hours'] = thisReview.get('author').get('playtime_at_review')/60 #Using playtime at review because it's more useful than playtime_forever
                 review['content'] = thisReview.get('review')
                 review['comments'] = thisReview.get('comment_count')
                 review['source'] = 'steam'
@@ -29,6 +29,15 @@ class ReviewHandler:
                 review['recommended'] = thisReview.get('voted_up')
                 review['franchise'] = self.franchise
                 review['gameName'] = self.gameName
+                reviewList.append(review)
+        self.reviews.extend(reviewList)
+
+    def saveReviews(self, filename):
+        file = open(filename, 'w')
+        file.write(json.dumps(self.reviews))
+        file.close()
+
+        
 
 
     #def
